18c48621a41036d41a2b8fa7369cc3e5c7eec66e
Wiput Pootong <wiput.pootong@gmail.com>
2019-05-02 18:51:28 +0700

    :sparkles: add booking list

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -4,16 +4,21 @@
     <v-content>
       <v-container fluid grid-list-lg>
         <v-layout row wrap>
-          <v-flex xs12 md4>
-            <v-card color="blue">
+          <v-flex xs12 md4 v-for="booking in bookings" :key="booking.id">
+            <v-card color="blue" dark>
               <v-card-title primary-title>
                 <div>
-                  <div class="headline">Unlimited music now</div>
-                  <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
+                  <div class="headline">Booking #{{ booking.id }}</div>
+                  <span>
+                    Date {{ booking.start_date }} to {{ booking.end_date }}
+                    <br>
+                    Total Room : {{ booking.detail.length }}
+                  </span>
                 </div>
               </v-card-title>
               <v-card-actions>
-                <v-btn flat dark>Listen now</v-btn>
+                <v-btn color="success" dark :to="`/booking/detail/${booking.id}`">Detail</v-btn>
+                <v-btn color="info" dark>Receipt</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -24,12 +29,19 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import store from '@/store/index'
 import NavBar from '@/components/NavBar.vue'
 
 export default {
   components: {
     NavBar
+  },
+  computed: {
+    ...mapState({
+      bookings: state => state.booking.bookings
+    })
   },
   beforeMount () {
     store.dispatch('booking/getBooking')
